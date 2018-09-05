d9c88e9df0116426f0c6612f6afd6bab25e0d0a4
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2018-09-05 18:25:54 +0200

    Camera works

--- src/pages/picture/picture.ts
+++ src/pages/picture/picture.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 @IonicPage()
 @Component({
   selector: 'page-picture',
@@ -8,10 +10,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PicturePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  image: String = "Pic";
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PicturePage');
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams, 
+              private camera: Camera) { }
+
+
+  
+
+  picture() {
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.image = base64Image;
+      }, (err) => {
+      // Handle error
+    });
   }
 }
