ca9161562d1f05abce7ab0fc967d98ca6771045a
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2018-09-12 12:24:51 +0200

    broken build

--- src/pages/picture/picture.ts
+++ src/pages/picture/picture.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ActionSheetController, ToastController, Platform, LoadingController, Loading, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, ToastController, Platform, LoadingController, Loading, AlertController, normalizeURL } from 'ionic-angular';
 
 import { Camera } from '@ionic-native/camera';
 import { File } from '@ionic-native/file';
 import { Transfer, TransferObject } from '@ionic-native/transfer';
 import { FilePath } from '@ionic-native/file-path';
+
+import { DomSanitizer } from '@angular/platform-browser';
 
 declare let cordova: any;
 
@@ -26,9 +28,10 @@ export class PicturePage {
     public toastCtrl: ToastController,
     public platform: Platform,
     public loadingCtrl: LoadingController,
-    private alertCtrl: AlertController) { }
+    private domSanitizer: DomSanitizer) { }
 
 
+  base64Image: String;
   lastImage: string = null;
   loading: Loading;
   cardImage: string = "../../assets/img/women_being_analyse_compressed.png";
@@ -81,10 +84,11 @@ export class PicturePage {
       } else {
         let currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
         let correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+
+        console.log(correctPath);
+
         this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
       }
-
-      this.uploadImage();
     }, (err) => {
       this.presentToast('Error while selecting image.');
     });
@@ -100,8 +104,9 @@ export class PicturePage {
 
   // Copy the image to a local folder
   private copyFileToLocalDir(namePath, currentName, newFileName) {
-    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, "").then(success => {
       this.lastImage = newFileName;
+      this.uploadImage();
     }, error => {
       this.presentToast('Error while storing file.');
     });
@@ -121,7 +126,7 @@ export class PicturePage {
     if (img === null) {
       return '';
     } else {
-      return cordova.file.dataDirectory + img;
+      return (cordova.file.dataDirectory + img).replace(/^file:\/\//, '');
     }
   }
 
@@ -129,8 +134,10 @@ export class PicturePage {
 
     let url = "http://yoururl/upload.php";
 
+    console.log(this.pathForImage(this.lastImage))
+
+    this.changeCardImage(this.pathForImage(this.lastImage));
     let targetPath = this.pathForImage(this.lastImage);
-    this.changeCardImage(targetPath);
 
     let filename = this.lastImage;
 
@@ -142,6 +149,7 @@ export class PicturePage {
       params: { 'fileName': filename }
     };
 
+    /*
     const fileTransfer: TransferObject = this.transfer.create();
 
     this.loading = this.loadingCtrl.create({
@@ -157,17 +165,10 @@ export class PicturePage {
       this.loading.dismissAll()
       this.presentToast('Error while uploading file.');
     });
+    */
   }
 
   private changeCardImage(targetPath) {
-
-
-    let alert = this.alertCtrl.create({
-      title: 'Low battery',
-      subTitle: targetPath,
-      buttons: ['Dismiss']
-    });
-    alert.present();
 
     this.cardImage = targetPath;
   }
