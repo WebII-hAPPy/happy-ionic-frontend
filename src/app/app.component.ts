2e91cb544b0dcc4546add1231a5391eeca27715a
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2018-10-04 18:23:43 +0200

    removed comments from template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,17 +48,15 @@ export class MyApp {
     private statusBar: StatusBar,
     private splashScreen: SplashScreen) {
 
-      platform.ready().then(() => {
-        // Okay, so the platform is ready and our plugins are available.
-        // Here you can do any higher level native things you might need.
-        this.statusBar.styleDefault();
-        this.splashScreen.hide();
-      });
+    platform.ready().then(() => {
+
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
+
     this.nav.setRoot(page.component);
   }
 }
