545b7262a5fc44c2fc85340a6f074fddf97a7d70
Michael Weidmann <michaelweidmann@web.de>
2018-10-12 22:49:17 +0200

    added all backend requests and processes of responses

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { AlertController, IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
-import {Api, User} from '../../providers';
-import {WelcomePage} from '../welcome/welcome';
-import {Storage} from "@ionic/storage";
+import { Api, User } from '../../providers';
+import { WelcomePage } from '../welcome/welcome';
+import { Storage } from "@ionic/storage";
+import { MainPage } from '..';
 
 @IonicPage()
 @Component({
@@ -11,7 +12,6 @@ import {Storage} from "@ionic/storage";
     templateUrl: 'settings.html'
 })
 export class SettingsPage {
-
 
     aboutSettings = {
         page: 'about',
@@ -22,24 +22,33 @@ export class SettingsPage {
     page: string = 'main';
     pageTitleKey: string = 'Settings';
 
-    name: string = '';
+    model = { name: '' };
 
     constructor(public navCtrl: NavController,
-                public navParams: NavParams,
-                public user: User,
-                private api: Api,
-                private storage: Storage,
-                private alertController: AlertController) {
+        public navParams: NavParams,
+        public user: User,
+        private api: Api,
+        private storage: Storage,
+        private alertController: AlertController,
+        private toastController: ToastController) {
     }
 
     /**
      * Changes the name of a user
      */
-    // TODO: correct endpoint; do not hardcode jwt_token; handle responses
-    changeName() {
+    changeName(): void {
+        const userId: number = this.user.getUser().id;
+
         this.storage.get('jwt_token').then((val) => {
-            this.api.put('/api/changeName', {'name': name}, { headers: { authorization: val } }).subscribe((response) => {
-                this.name = '';
+            this.api.put('api/changeName/' + userId, this.model, { headers: { authorization: val } }).subscribe((response) => {
+                this.navCtrl.push(MainPage).then(() => this.presentToast('Successfully updated your name.'));
+            }, err => {
+                if (err.status === 401) {
+                    this.storage.clear();
+                    this.navCtrl.push('WelcomePage').then(() => this.presentToast('You are not logged in...'));
+                } else {
+                    this.presentToast('Could not update your name.');
+                }
             });
         });
     }
@@ -47,11 +56,21 @@ export class SettingsPage {
     /**
      * Deletes a account
      */
-    // TODO: correct endpoint; do not hardcode jwt_token; handle response
-    deleteAccount() {
+    deleteAccount(): void {
+        const userId: number = this.user.getUser().id;
+
         this.storage.get('jwt_token').then((val) => {
-            this.api.delete('/api/delete', { headers: { authorization: val } }).subscribe((response) => {
-                this.navCtrl.push(WelcomePage);
+            this.api.delete('api/deleteAccount/' + userId, { headers: { authorization: val } }).subscribe((response) => {
+                this.storage.clear();
+
+                this.navCtrl.push(WelcomePage).then(() => this.presentToast('Your account was successfully deleted.'));
+            }, err => {
+                if (err.status === 401) {
+                    this.storage.clear();
+                    this.navCtrl.push('WelcomePage').then(() => this.presentToast('You are not logged in...'));
+                } else {
+                    this.presentToast('Could not delete your account.');
+                }
             });
         })
     }
@@ -59,7 +78,7 @@ export class SettingsPage {
     /**
      * Shows a alert to ask the user if he really wants to delete his account.
      */
-    showAlert() {
+    showAlert(): void {
         let alert = this.alertController.create({
             title: 'Confirm deletion',
             message: 'Are you sure you want to delete your account?',
@@ -86,11 +105,24 @@ export class SettingsPage {
     }
 
     /**
+   * Show a message with a toast.
+   * @param text Message
+   */
+    private presentToast(text: string): void {
+        let toast = this.toastController.create({
+            message: text,
+            duration: 3000,
+            position: 'top'
+        });
+        toast.present();
+    }
+
+    /**
      * Runs when the page has finished leaving and is no longer the active page.
      */
     ionViewWillEnter(): void {
         this.page = this.navParams.get('page') || this.page;
         this.pageTitleKey = this.navParams.get('pageTitleKey') || this.pageTitleKey;
-        this.name = '';
+        this.model.name = '';
     }
 }
