5670ba91c0065c7d31ddb22e245fe10dc2ea8e4a
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-10-21 21:48:06 +0200

    .

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,29 +1,38 @@
-import { Component } from '@angular/core';
-import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+    AlertController,
+    IonicPage,
+    NavController,
+    NavParams
+} from "ionic-angular";
 
-import { Api, User, Utils } from '../../providers';
-import { WelcomePage } from '../welcome/welcome';
+import { Api, User, Utils } from "../../providers";
+import { WelcomePage } from "../welcome/welcome";
 import { Storage } from "@ionic/storage";
-import { MainPage } from '..';
-import { global_401Error, global_500Error, settings_accountDeleted, settings_accountDeletedError } from '../../providers/utils/strings';
+import { MainPage } from "..";
+import {
+    global_401Error,
+    global_500Error,
+    settings_accountDeleted,
+    settings_accountDeletedError
+} from "../../providers/utils/strings";
 
 @IonicPage()
 @Component({
-    selector: 'page-settings',
-    templateUrl: 'settings.html'
+    selector: "page-settings",
+    templateUrl: "settings.html"
 })
 export class SettingsPage {
-
     aboutSettings = {
-        page: 'about',
-        pageTitleKey: 'about'
+        page: "about",
+        pageTitleKey: "about"
     };
     about: any = SettingsPage;
 
-    page: string = 'main';
-    pageTitleKey: string = 'Settings';
+    page: string = "main";
+    pageTitleKey: string = "Settings";
 
-    model = { name: '' };
+    model = { name: "" };
 
     constructor(
         public navCtrl: NavController,
@@ -32,8 +41,8 @@ export class SettingsPage {
         private api: Api,
         private storage: Storage,
         private alertController: AlertController,
-        private utils: Utils) {
-    }
+        private utils: Utils
+    ) {}
 
     /**
      * Changes the name of a user
@@ -41,19 +50,38 @@ export class SettingsPage {
     changeName(): void {
         const userId: number = this.user.getUser().id;
 
-        this.storage.get('jwt_token').then((val) => {
-            this.api.put('api/changeName/' + userId, this.model, { headers: { authorization: val } }).subscribe((response) => {
-                this.navCtrl.push(MainPage).then(() => this.utils.presentToast('Successfully updated your name.'));
-            }, err => {
-                if (err.status === 401) {
-                    this.storage.clear();
-                    this.navCtrl.push('WelcomePage').then(() => this.utils.presentToast(global_401Error));
-                } else if (err.status === 500 || err.status === 502) {
-                    this.utils.presentToast(global_500Error);
-                } else {
-                    this.utils.presentToast('Could not update your name.');
-                }
-            });
+        this.storage.get("jwt_token").then(val => {
+            this.api
+                .put("api/changeName/" + userId, this.model, {
+                    headers: { authorization: val }
+                })
+                .subscribe(
+                    response => {
+                        this.navCtrl
+                            .push(MainPage)
+                            .then(() =>
+                                this.utils.presentToast(
+                                    "Successfully updated your name."
+                                )
+                            );
+                    },
+                    err => {
+                        if (err.status === 401) {
+                            this.storage.clear();
+                            this.navCtrl
+                                .push("WelcomePage")
+                                .then(() =>
+                                    this.utils.presentToast(global_401Error)
+                                );
+                        } else if (err.status === 500 || err.status === 502) {
+                            this.utils.presentToast(global_500Error);
+                        } else {
+                            this.utils.presentToast(
+                                "Could not update your name."
+                            );
+                        }
+                    }
+                );
         });
     }
 
@@ -63,22 +91,39 @@ export class SettingsPage {
     deleteAccount(): void {
         const userId: number = this.user.getUser().id;
 
-        this.storage.get('jwt_token').then((val) => {
-            this.api.delete('api/deleteAccount/' + userId, { headers: { authorization: val } }).subscribe((response) => {
-                this.storage.clear();
+        this.storage.get("jwt_token").then(val => {
+            this.api
+                .delete("api/deleteAccount/" + userId, {
+                    headers: { authorization: val }
+                })
+                .subscribe(
+                    response => {
+                        this.storage.clear();
 
-                this.navCtrl.push(WelcomePage).then(() => this.utils.presentToast(settings_accountDeleted));
-            }, err => {
-                if (err.status === 401) {
-                    this.storage.clear();
-                    this.navCtrl.push('WelcomePage').then(() => this.utils.presentToast(global_401Error));
-                } else if (err.status === 500 || err.status === 502) {
-                    this.utils.presentToast(global_500Error);
-                } else {
-                    this.utils.presentToast(settings_accountDeletedError);
-                }
-            });
-        })
+                        this.navCtrl
+                            .push(WelcomePage)
+                            .then(() =>
+                                this.utils.presentToast(settings_accountDeleted)
+                            );
+                    },
+                    err => {
+                        if (err.status === 401) {
+                            this.storage.clear();
+                            this.navCtrl
+                                .push("WelcomePage")
+                                .then(() =>
+                                    this.utils.presentToast(global_401Error)
+                                );
+                        } else if (err.status === 500 || err.status === 502) {
+                            this.utils.presentToast(global_500Error);
+                        } else {
+                            this.utils.presentToast(
+                                settings_accountDeletedError
+                            );
+                        }
+                    }
+                );
+        });
     }
 
     /**
@@ -86,15 +131,15 @@ export class SettingsPage {
      */
     showDeleteAccountAlert(): void {
         let alert = this.alertController.create({
-            title: 'Confirm deletion',
-            message: 'Are you sure you want to delete your account?',
+            title: "Confirm deletion",
+            message: "Are you sure you want to delete your account?",
             buttons: [
                 {
-                    text: 'Cancel',
-                    role: 'cancel'
+                    text: "Cancel",
+                    role: "cancel"
                 },
                 {
-                    text: 'Delete',
+                    text: "Delete",
                     handler: () => this.deleteAccount()
                 }
             ]
@@ -110,12 +155,16 @@ export class SettingsPage {
         this.navCtrl.push(WelcomePage);
     }
 
+    changePassword(): void {
+    }
+
     /**
      * Runs when the page is about to enter and becomes the active page.
      */
     ionViewWillEnter(): void {
-        this.page = this.navParams.get('page') || this.page;
-        this.pageTitleKey = this.navParams.get('pageTitleKey') || this.pageTitleKey;
-        this.model.name = '';
+        this.page = this.navParams.get("page") || this.page;
+        this.pageTitleKey =
+            this.navParams.get("pageTitleKey") || this.pageTitleKey;
+        this.model.name = "";
     }
 }
