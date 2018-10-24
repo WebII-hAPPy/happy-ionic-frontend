1e522a8090b183bc92175dc3f20f277a439e07df
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2018-10-24 18:52:36 +0200

    Email validator

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -40,7 +40,7 @@ export class RegisterPage {
                     "",
                     [
                         Validators.required,
-                        CustomFormValidator.validateEmail(/^.+@.+$/)
+                        CustomFormValidator.validateEmail(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
                     ]
                 ],
                 password: ["", [Validators.required]],
