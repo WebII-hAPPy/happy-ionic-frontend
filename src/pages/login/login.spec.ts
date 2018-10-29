e84d6fcdb5fd1be2d45aabd64c4f4f2fe1403600
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-10-29 17:51:46 +0100

    .

--- src/pages/login/login.spec.ts
+++ src/pages/login/login.spec.ts
@@ -2,11 +2,6 @@ import { HttpClientModule } from "@angular/common/http";
 import { DebugElement, ErrorHandler } from "@angular/core";
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { BrowserModule, By } from "@angular/platform-browser";
-import { Camera } from "@ionic-native/camera";
-import { FilePath } from "@ionic-native/file-path";
-import { SplashScreen } from "@ionic-native/splash-screen";
-import { StatusBar } from "@ionic-native/status-bar";
-import { Transfer } from "@ionic-native/transfer";
 import { IonicStorageModule } from "@ionic/storage";
 import {
     IonicErrorHandler,
@@ -14,28 +9,18 @@ import {
     NavController,
     NavParams
 } from "ionic-angular";
-import { ChartsModule } from "ng2-charts";
 import { MyApp } from "../../app/app.component";
-import { Api, Face, User, Utils } from "../../providers";
-import { AnalysisPage } from "../analysis/analysis";
+import { Api, User, Utils } from "../../providers";
 import { LoginPage } from "../login/login";
-import { PicturePage } from "../picture/picture";
-import { RegisterPage } from "../register/register";
-import { SettingsPage } from "../settings/settings";
-import { StatsPage } from "../stats/stats";
-import { WelcomePage } from "../welcome/welcome";
 
 describe("LoginPage", () => {
-    let component: LoginPage;
-    let fixture: ComponentFixture<LoginPage>;
+    let component: any;
+    let fixture: ComponentFixture<any>;
     let de: DebugElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [
-                MyApp,
-                LoginPage
-            ],
+            declarations: [MyApp, LoginPage],
             imports: [
                 BrowserModule,
                 HttpClientModule,
